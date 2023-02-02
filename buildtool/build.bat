555a78829a96cc657c0aba753a791563c9457a58
Lition802 <2959435045@qq.com>
2023-02-02 10:41:30 +0800

    update

---
+++ buildtool/build.bat
@@ -0,0 +1,17 @@
+cd ../../
+
+md tmp
+
+md "tmp/sparkbridge"
+
+xcopy sparkbridge "tmp/sparkbridge" /s /e /exclude:%cd%\sparkbridge\buildtool\pack.config
+
+cd ./sparkbridge/buildtool
+
+7za.exe a ../../tmp/sparkbridge.zip ../../tmp/sparkbridge/*
+
+cd ../../tmp
+
+REN sparkbridge.zip sparkbridge.llplugin
+
+pause
